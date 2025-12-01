26b6f316f7d3645d9f5ee0d8141468d6d13e69ec
DESKTOP-8CTAJ6U\쥐마 <jeonji1101@gmail.com>
2025-12-02 02:04:10 +0900

    definefunction

--- main.py
+++ main.py
@@ -2,3 +2,20 @@ import tkinter as tk
 from tkinter import ttk, messagebox, filedialog # GUI 위젯, 메시지 박스, 파일 대화상자
 import json # JSON 데이터 처리
 import pandas as pd # CSV, Excel 데이터 처리
+
+class StudentManagerApp:
+    def __init__(self, root):
+        pass
+
+
+    def create_widgets(self):
+        pass 
+
+    def add_student(self):
+        pass
+
+    def delete_student(self):
+        pass
+
+
+    

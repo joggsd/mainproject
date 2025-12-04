11e5bcd42eb5554398a563e51dfc268fbbc54405
DESKTOP-8CTAJ6U\쥐마 <jeonji1101@gmail.com>
2025-12-04 22:56:15 +0900

    addinputframe

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ import pandas as pd # CSV, Excel 데이터 처리
 
 class StudentManagerApp:
     def __init__(self, root):
-        self.root = root
-        self.root.title("학생 관리 시스템")
-        self.root.geometry("600x420")
+        self.root = root # 메인 윈도우
+        self.root.title("학생 관리 시스템") # 윈도우 제목 설정
+        self.root.geometry("600x420") # 윈도우 크기 설정
 
-        self.create_widgets()
 
-        
-       
+        self.create_widgets() # 위젯 생성 함수 호출
+           
     def create_widgets(self):
-        pass 
+        input_frame = ttk.LabelFrame(self.root, text="학생 정보 입력")
+        input_frame.pack(fill="x", padx=10, pady=10)
+
+
+     
 
     def add_student(self):
         pass
